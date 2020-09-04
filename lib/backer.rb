8c022b1f7029436b43b5c022400d7a9a0936fa32
Nancy Jackson <nbjackson23@gmail.com>
2020-09-04 14:55:25 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,11 +1,14 @@
 class Backer
   
-  attr_reader :name
+  attr_reader :name :backed_projects
   
   @@backed_projects = []
   
   def initialize(name)
   @name =name
   end
-
+  
+  def back_project()
+    @@backed_projects
+  end
 end
